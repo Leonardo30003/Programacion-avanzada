0e8a0b602bc8f4a728833439fb02bca9ece1d548
Leonardo Carrion <lecarrionca@uide.edu.ec>
2022-10-28 08:41:21 -0500

    First code

---
+++ Fundamentos/operacionesMatematicas.py
@@ -0,0 +1,19 @@
+"""
+Ejemplos de operacione matematicas
+"""
+
+suma=1+2
+resta= 2-1
+division= 10/2
+multiplicacion=9*2
+pontenciacion=8**3
+residuo=5%4
+divisionExacta=20//2
+
+print(suma)
+print(resta)
+print(division)
+print(multiplicacion)
+print(residuo)
+print(pontenciacion)
+print(divisionExacta)
